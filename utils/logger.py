1243bc97997e19555155ae47100ee5d79925b975
dropcodes <97192664+dropcreations@users.noreply.github.com>
2023-11-27 00:37:50 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -3,6 +3,9 @@ import sys
 import logging
 
 from datetime import datetime, date
+from rich.console import Console
+
+cons = Console()
 
 def __get_path():
     if getattr(sys, 'frozen', False):
@@ -15,41 +18,25 @@ def __get_path():
         )
 
 LOGSDIR = os.path.join(__get_path(),'logs')
-os.makedirs(LOGSDIR, exist_ok=True)
+
+if not os.path.exists(LOGSDIR):
+    os.makedirs(LOGSDIR)
 
 LOGFILE = os.path.join(
     LOGSDIR,
-    f'manzana_am_log_{date.today().strftime("%y%m%d")}{datetime.now().strftime("%H%M%S")}.log'
+    'manzana_aplm_log_{}{}.log'.format(
+        date.today().strftime("%y%m%d"),
+        datetime.now().strftime("%H%M%S")
+    )
 )
-
-fmts = {
-    logging.DEBUG: '\033[32;1m[{asctime}]\033[0m \033[33;1m[Manzana]\033[0m \033[32;1mDEBUG:\033[0m \033[1;37m{message}\033[0m',
-    logging.INFO: '\033[32;1m[{asctime}]\033[0m \033[33;1m[Manzana]\033[0m \033[38;5;39;1mINFO:\033[0m \033[1;37m{message}\033[0m',
-    logging.WARNING: '\033[32;1m[{asctime}]\033[0m \033[33;1m[Manzana]\033[0m \033[31;1mWARN:\033[0m \033[1;37m{message}\033[0m',
-    logging.ERROR: '\033[32;1m[{asctime}]\033[0m \033[33;1m[Manzana]\033[0m \033[31;1mERROR:\033[0m \033[1;37m{message}\033[0m',
-    logging.CRITICAL: '\033[32;1m[{asctime}]\033[0m \033[33;1m[Manzana]\033[0m \033[31;1mCRITICAL:\033[0m \033[1;37m{message}\033[0m'
-}
-
-class ManzanaFormatter(logging.Formatter):
-    def format(self, record):
-        logfmt = fmts[record.levelno]
-        formatter = logging.Formatter(
-            logfmt,
-            datefmt='%d-%m-%y %H:%M:%S',
-            style='{'
-        )
-        return formatter.format(record)
         
 def __get_logger():
-    streamHandler = logging.StreamHandler()
-    streamHandler.setFormatter(ManzanaFormatter())
-    streamHandler.setLevel(logging.INFO)
-
     fileHandler = logging.FileHandler(
         filename=LOGFILE,
         mode='w',
         encoding='utf8'
     )
+
     fileHandler.setFormatter(
         logging.Formatter(
             fmt='[{asctime}] [Manzana] {levelname}: {message}',
@@ -57,11 +44,12 @@ def __get_logger():
             style='{'
         )
     )
+    
     fileHandler.setLevel(logging.DEBUG)
 
     logging.basicConfig(
         level=logging.DEBUG,
-        handlers=[streamHandler, fileHandler]
+        handlers=[fileHandler]
     )
 
     return logging.getLogger()
@@ -69,15 +57,33 @@ def __get_logger():
 logger = __get_logger()
 
 def info(msg, exit=0):
+    now = datetime.now()
+    now = now.strftime("%H:%M:%S")
+    log = f"[bold green][{now}][/] [bold yellow][ManzanaCore][/] [deep_sky_blue1]INFO:[/] [default]{msg}[/]"
+
+    cons.print(log)
     logger.info(msg)
+
     if exit: sys.exit(1)
 
 def error(msg, exit=0):
+    now = datetime.now()
+    now = now.strftime("%H:%M:%S")
+    log = f"[bold green][{now}][/] [bold yellow][ManzanaCore][/] [bold red]ERROR:[/] [default]{msg}[/]"
+
+    cons.print(log)
     logger.error(msg)
+
     if exit: sys.exit(1)
 
 def warning(msg, exit=0):
+    now = datetime.now()
+    now = now.strftime("%H:%M:%S")
+    log = f"[bold green][{now}][/] [bold yellow][ManzanaCore][/] [bold red]WARN:[/] [default]{msg}[/]"
+
+    cons.print(log)
     logger.warning(msg)
+
     if exit: sys.exit(1)
 
 def debug(msg):
