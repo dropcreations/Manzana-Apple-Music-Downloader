67d3a37afddff9edc8b052e2b6faf822e9c6c5fa
dropcodes <97192664+dropcreations@users.noreply.github.com>
2023-11-27 00:35:31 +0530

    Update animartwork.py

--- core/content/animartwork.py
+++ core/content/animartwork.py
@@ -7,12 +7,44 @@ from utils import logger
 
 console = Console()
 
+def get_variants(data: dict):
+    if len(data.keys()) > 1:
+        ids = []
+
+        table = Table(box=box.ROUNDED)
+
+        table.add_column("ID", justify="center")
+        table.add_column("Type", justify="center")
+
+        for i, stream in enumerate(list(data.keys())):
+            ids.append(i)
+            table.add_row(
+                str(i),
+                stream
+            )
+
+        print()
+        columns = Columns(["       ", table])
+        console.print(columns)
+        id = input("\n\t Enter ID: ")
+        print()
+        
+        if id == "": logger.error("Please enter an ID to continue!", 1)
+        else:
+            try: id = int(id)
+            except: logger.error("Invalid input!", 1)
+
+        if id in ids: return data[list(data.keys())[id]]
+        else: logger.error("ID not found in the list!", 1)
+    else:
+        return data[list(data.keys())[0]]
+
 def get_animartwork(data):
     if "animartwork" in data:
         logger.info("Getting animated artwork variants...")
 
         ids = []
-        streamList = data.get("animartwork")
+        streamList = get_variants(data["animartwork"])
 
         table = Table(box=box.ROUNDED)
 
@@ -22,6 +54,8 @@ def get_animartwork(data):
         table.add_column("Resolution", justify="left")
         table.add_column("FPS", justify="center")
         table.add_column("Range", justify="center")
+
+        logger.info("Getting animated artwork streams...")
 
         for i, stream in enumerate(streamList):
             ids.append(i)
